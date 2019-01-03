b1bdf572073c7735247b5d2e6f4491b8c6c4f178
vietnguyenduc <nguyenducviet1998tk@gmail.com>
2019-01-03 20:31:46 +0700

    up

---
+++ data/data.js
@@ -0,0 +1,24 @@
+var data = [
+    {
+        id:1,
+        name: "",
+        type:"",
+        price:0,
+        original:0,
+        image:"",
+        rate:5,
+        like:12,
+        comment:15
+    },
+    {
+        id:2,
+        name: "",
+        type:"",
+        price:0,
+        original:0,
+        image:"",
+        rate:5,
+        like:12,
+        comment:15
+    }
+]
